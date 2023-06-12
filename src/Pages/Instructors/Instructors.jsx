eab243ebaa2fcf661a748e215fbd2f63f09b3574
nikkonbd <nikkon1998@gmail.com>
2023-06-12 21:23:33 +0600

    popular added

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,6 +1,5 @@
 // import React from 'react';
 
-// style = "background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
 import useInstructors from '../../hooks/useInstructors';
 import './Instructors.css'
 import InstructorsCard from './InstructorsCard';
